27bd8cdf7881b69f14a76bc6b9c940ea1031d222
James Lambert <james.lambert3995@gmail.com>
2026-06-02 14:49:33 -0500

    Making changes that should fix deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_PAGES === "true"
+const repo = "armv7-encoder"
+
 const nextConfig = {
+  output: "export",
+  basePath: isGithubPages ? `/${repo}` : "",
+  assetPrefix: isGithubPages ? `/${repo}/` : "",
   typescript: {
     ignoreBuildErrors: true,
   },
